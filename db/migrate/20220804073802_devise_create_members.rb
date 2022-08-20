f40ae5833de9b3077c6dcd3a25fefbc2f3fbc0b1
EC2 Default User <ec2-user@ip-172-31-40-221.ap-northeast-1.compute.internal>
2022-08-20 07:23:57 +0000

    [final] 35

--- db/migrate/20220804073802_devise_create_members.rb
+++ db/migrate/20220804073802_devise_create_members.rb
@@ -8,7 +8,7 @@ class DeviseCreateMembers < ActiveRecord::Migration[6.1]
       t.string :encrypted_password, null: false, default: ""
       t.string :name,               null: false, default: ""
       t.string :nick_name,          null: false, default: "デフォルト"
-      t.text :self_introduction,    default: "よろしくお願いします。"
+      t.text :self_introduction
       t.boolean :is_deleted,        null: false, default: "false"
 
       ## Recoverable
